04a5059b16bc08d7e3a89adc67c551704ae2fad5
Ben Upenieks <ben.upenieks@gmail.com>
2017-06-02 15:59:47 -0700

    Added directional linked lists to link contiguous tiles. Added function to evaluate valid tile placement. Increased flexibility of api and created functions allowing pseudo method overloading.

--- Game_Library.js
+++ Game_Library.js
@@ -1,7 +1,11 @@
+const NUM_ROWS = 15;
+const NUM_COLUMNS = 15;
+const NUM_TILES = NUM_ROWS * NUM_COLUMNS;
+const NUM_TRAY_TILES = 7;
+const BORDER_WIDTH = 1;
 
-function tile(_id, _isFree, _character, _score) {
+function tile(_id, _character, _score) {
 	this.id = _id;
-	this.isFree = _isFree;
 	this.letter = {
 		character: _character,
 		score: _score
@@ -9,10 +13,61 @@ function tile(_id, _isFree, _character, _score) {
 	//$("#" + this.id).html("<p>" + this.letter.character + "</p>");
 };
 
+var UI = (function() {
+
+	return {
+		init: function() {
+			var boardEl = $("#board");
+			for (var i = 0; i < NUM_ROWS; i++) {
+				boardEl.append("<div id='row" + i + "' class='row'></div>");
+				var curRow = boardEl.children("#row" + i);
+				for (var j = 0; j < NUM_COLUMNS; j++) {
+					var id = 'tile' + (i+1) + (j+1);
+					curRow.append("<div id='" + id + "' class='tile'></div>");
+				} 
+				Board.setTileDroppable(curRow.find(".tile"));
+			} 
+
+			var tray = $("#tray");
+			for (var i = 0; i < NUM_TRAY_TILES; i++) {
+				var id = "tray-tile" + i;
+				tray.append("<div id='tray-tile" + i + "' class='tile' class='current-turn-tiles'></div>");
+				Board.setTrayTile(i, new tile(id, "A", 12));
+			}
+
+			this.resizeUI();
+		},
+
+		resizeUI: function() {
+
+			var boardEl = $("#board");
+			var rows = $(".row");
+			var tiles = $(".tile");
+			var tray = $("#tray");
+
+			var boardLength = boardEl.width();
+			var rowHeight = boardLength / NUM_ROWS - BORDER_WIDTH;
+			var tileWidth = boardLength / NUM_COLUMNS - BORDER_WIDTH;
+
+			boardEl.css('height', boardLength);
+			rows.css('height', rowHeight);
+			rows.css('width', boardLength);
+			tiles.css('height', rowHeight);
+			tiles.css('width', tileWidth);
+			tray.css('height', rowHeight);
+			tray.css('width', (tileWidth + BORDER_WIDTH) * NUM_TRAY_TILES);
+		},
+
+
+	}
+
+})();
+
 var Board = (function() {
 
 	// Private
 	var tileMap = new Map();
+	var currentTurnTiles = new Array();
 
 	// Public
 	return {
@@ -52,27 +107,73 @@ var Board = (function() {
 
 		tileMoved: function(source_id, target_id) {
 			this.setTileById(target_id, this.getTileById(source_id));
+
+			tileMap.delete(source_id);
 		},
 
 		setTile: function(_row, _column, _tile) {
 			let id = "tile" + _row + _column;
 			tileMap.set(id, _tile);
 			var char = this.getTileLetter(_row, _column).character;
-			if (char != null) $("#" + id).html("<p>" + char + "</p>");
+			if (char != null) $("#" + id).html("<p>" + char + "</p>").show();
 		},
 
-		setTileById: function(_id, _tile) {
-			tileMap.set(_id, _tile);
+		setTileById: function(_target, _tile) {
+			target = this.idDomOrPos(_target);
+			tileMap.set(target.attr('id'), _tile);
 			var char = _tile.letter.character;
-			console.log(_id + " -- character: " + char);
-			if (char) $("#" + _id).html("<p>" + char + "</p>");
+			console.log(target + " -- character: " + char);
+			if (char) target.html("<p>" + char + "</p>").show();
 		},
 
 		setTrayTile: function(_number, _tile) {
 			let id = "tray-tile" + _number;
 			tileMap.set(id, _tile);
 			var char = _tile.letter.character;
-			if (char) $("#" + id).html("<p>" + char + "</p>");
+			if (char) $("#" + id).html("<p>" + char + "</p>").show();
+		},
+
+		setTileDraggable: function(target) {
+			el = this.idDomOrPos(target);
+			if (el.draggable("instance")) {		// If there' already an instance,
+				el.draggable("enable");			// just enable.
+			} else {
+				el.draggable({
+					revert: true,
+					revertDuration: 0
+				});
+			}
+		},
+
+		setTileDroppable: function(target) {
+			el = this.idDomOrPos(target);
+			el.droppable({
+				drop: function(event, ui) {
+					var targetId = $(this).attr('id');
+					if (tileMap.has(targetId)) { 
+						return; // space is occupied -- do nothing.
+					}
+					var sourceId = ui.draggable.attr('id');
+					Board.tileMoved(sourceId, targetId);
+					Board.setBlankSpace(sourceId);
+					Board.setTileDraggable(targetId);
+					console.log(sourceId + " dropped. "
+						+ targetId + " now draggable");
+				}
+			});
+		},
+
+		setBlankSpace: function(_id) {
+			$("#" + _id).html("").draggable("disable");
+		},
+
+		//Terrible, nifty code
+		idDomOrPos(callerParam) {	//Allows functions to take in multiple types
+			return jQuery.type(callerParam) === "string" ?
+						$("#" + callerParam) : 
+						(jQuery.type(callerParam) === "array" ? 
+						$("#tile" + callerParam[0] + callerParam[1]) :
+						callerParam );
 		}
 	};
 
@@ -83,11 +184,7 @@ var Game = (function () {
 	//public
 	return{
 		startTurn: function() {
-			for (let i = 0; i < NUM_TRAY_TILES; i++) {
-				$("#tray-tile" + i ).draggable({
-					revert: true
-				});
-			}
+			Board.setTileDraggable($("#tray > div"));
 		}
 	};
 })();
--- UI.js
+++ UI.js
@@ -1,66 +1,13 @@
-const NUM_ROWS = 15;
-const NUM_COLUMNS = 15;
-const NUM_TILES = NUM_ROWS * NUM_COLUMNS;
-const NUM_TRAY_TILES = 7;
-const BORDER_WIDTH = 1;
-
-$(document).ready(function() {
-	var boardEl = $("#board");
-	for (var i = 0; i < NUM_ROWS; i++) {
-		boardEl.append("<div id='row" + i + "' class='row'></div>");
-		for (var j = 0; j < NUM_COLUMNS; j++) {
-			var id = 'tile' + (i+1) + (j+1);
-			$("#row" + i).append("<div id='" + id + "' class='tile'></div>");	
-			Board.setTile(i, j, new tile(id, true, null, 0));
-			console.log("first: "+ id);
-			$("#" + id).droppable({
-				drop: function(event, ui) {
-					var sourceId = ui.draggable.attr('id');
-					console.log(id);
-					Board.tileMoved(sourceId, $(this).attr('id'));
-					ui.draggable.hide();
-				}
-			});
-
-		} 
-	} 
-
-	var tray = $("#tray");
-	for (var i = 0; i < NUM_TRAY_TILES; i++) {
-		var id = "tray-tile" + i;
-		tray.append("<div id='tray-tile" + i + "' class='tile' class='current-turn-tiles'></div>");
-		Board.setTrayTile(i, new tile(id, true, "A", 12));
-	}
-
-	resizeUI();
-	Game.startTurn();
-
-});
-
-
-function resizeUI() {
-	var boardEl = $("#board");
-	var rows = $(".row");
-	var tiles = $(".tile");
-	var tray = $("#tray");
-
-	var boardLength = boardEl.width();
-	var rowHeight = boardLength / NUM_ROWS - BORDER_WIDTH;
-	var tileWidth = boardLength / NUM_COLUMNS - BORDER_WIDTH;
-
-	boardEl.css('height', boardLength);
-	rows.css('height', rowHeight);
-	rows.css('width', boardLength);
-	tiles.css('height', rowHeight);
-	tiles.css('width', tileWidth);
-	tray.css('height', rowHeight);
-	tray.css('width', (tileWidth + BORDER_WIDTH) * NUM_TRAY_TILES);
-};
 
 var resizeTimer;
 $(window).resize(function() {
 	clearTimeout(resizeTimer);
-	resizeTimer = setTimeout(resizeUI, 50);
+	resizeTimer = setTimeout(UI.resizeUI, 50);
 })
+
+$(document).ready(function() {
+	UI.init();
+	Game.startTurn();
+});
 
 

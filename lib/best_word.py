faafc4ee7f57ae6e51685fb1853efad49b0943b0
Bupenieks <Ben.upenieks@gmail.com>
2017-07-13 23:49:01 -0400

    New python func to check characters are contiguous

--- lib/best_word.py
+++ lib/best_word.py
@@ -1,4 +1,10 @@
 import sys, os, json
+from itertools import takewhile, dropwhile
+
+
+# Constants-----
+SPACE = ' '
+#---------------
 
 def getPermutations(word):
   def recurse(word, perms, retPerm = []):
@@ -15,6 +21,7 @@ def getPermutations(word):
   recurse(word, perms)
   return perms
    
+
 def powerSet(word):
   ans = set()
   if (len(word) == 0):
@@ -29,12 +36,31 @@ def powerSet(word):
 
 def hasCharacter(word):
   for char in word[3:]:
-    if char is not ' ':
+    if char is not SPACE:
       return True
   return False
 
 
+def isConnected(line):
+  charCount = 0
+  for char in line:
+    if char is not SPACE:
+      charCount += 1 
+  removedLeadingSpaces = [i for i in dropwhile(lambda x: x is SPACE, line)]
+  charString = [x for x in takewhile(lambda x: x is not SPACE, removedLeadingSpaces)]
+  if charCount is not len(charString):
+    return False
+  return True
+
 sys.argv[:] = [word for word in sys.argv if hasCharacter(word)]
+sys.argv = sys.argv[1:] #Gets rid of unnecessary first arg
+trayTiles = sys.argv[0];
+allPerms = [getPermutations(x) for x in powerSet(trayTiles)] 
+allPerms = [perm for sets in allPerms for perm in sets]
+
+for word in sys.argv:
+  chars = list(word)
+  # Permute line
 
 for i in range(len(sys.argv)):
   if i is 0: 
